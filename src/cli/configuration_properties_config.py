9c6929e37ea090579e37b5d6e7b6626211957cef
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2023-08-10 12:02:57 +0200

    Read sonar config from pyproject.toml (#7)

--- src/cli/configuration_properties_config.py
+++ src/cli/configuration_properties_config.py
@@ -1,5 +1,8 @@
 import argparse
+import os.path
 import sys
+
+import toml
 
 from cli.context import Context
 
@@ -41,4 +44,25 @@ class ConfigurationPropertiesConfig:
         for arg in sys.argv:
             if arg.startswith("-D"):
                 scan_arguments.append(arg)
+
         ctx.scan_arguments = scan_arguments
+        if not os.path.isfile('pyproject.toml'):
+            return
+
+        with open('pyproject.toml', 'r') as file:
+            # TODO: actually search for pyproject.toml
+            toml_data = file.read()
+            parsed_data = toml.loads(toml_data)
+            print(parsed_data)
+            if 'sonar' in parsed_data:
+                sonar_properties = parsed_data['sonar']
+                for key, value in sonar_properties.items():
+                    add_parameter_to_scanner_args(scan_arguments, key, value)
+
+
+def add_parameter_to_scanner_args(scan_arguments: list[str], key: str, value: str | dict):
+    if isinstance(value, str):
+        scan_arguments.append(f"-Dsonar.{key}={value}")
+    if isinstance(value, dict):
+        for k, v in value.items():
+            add_parameter_to_scanner_args(scan_arguments, f"{key}.{k}", v)
