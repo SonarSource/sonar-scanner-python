397878c9050ca7e520d7a5431f5c91806c639dff
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2023-08-10 12:02:22 +0200

    Redirect stderr as well as stdout (#8)

--- src/cli/scanner.py
+++ src/cli/scanner.py
@@ -9,19 +9,18 @@ class Scanner:
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
 
-        def print_output():
-            while True:
-                # TODO: print error as well
-                output_line = process.stdout.readline()
-                if not output_line:
-                    break
-                decoded_line = output_line.decode('utf-8')
+        def print_output(stream):
+            for line in stream:
+                decoded_line = line.decode('utf-8')
                 print(decoded_line, end='', flush=True)
 
-        output_thread = threading.Thread(target=print_output)
+        output_thread = threading.Thread(target=print_output, args=(process.stdout,))
+        error_thread = threading.Thread(target=print_output, args=(process.stderr,))
         output_thread.start()
+        error_thread.start()
 
         process.wait()
         output_thread.join()
+        error_thread.join()
 
         return process.returncode
