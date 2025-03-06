d6c5be1a491c317dfe76aad3b2a5b01269cb7ccf
Sebastian Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2025-03-06 11:04:32 +0100

    add SQVersion class

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,12 +19,37 @@
 #
 import unittest
 
-from pysonar_scanner.utils import remove_trailing_slash
+from pysonar_scanner.utils import SQVersion, remove_trailing_slash
 
 
-class UtilsTest(unittest.TestCase):
+class TestUtils(unittest.TestCase):
     def test_removing_trailinlg_slash(self):
         self.assertEqual(remove_trailing_slash("test/"), "test")
         self.assertEqual(remove_trailing_slash(" test/ "), "test")
         self.assertEqual(remove_trailing_slash(" test / "), "test")
         self.assertEqual(remove_trailing_slash("test"), "test")
+
+
+class TestSQVersion(unittest.TestCase):
+    def test_does_support_bootstrapping(self):
+        self.assertTrue(SQVersion.from_str("10.6").does_support_bootstrapping())
+        self.assertTrue(SQVersion.from_str("10.6.5").does_support_bootstrapping())
+        self.assertTrue(SQVersion.from_str("10.6.5a").does_support_bootstrapping())
+        self.assertTrue(SQVersion.from_str("10.7").does_support_bootstrapping())
+        self.assertTrue(SQVersion.from_str("11.1").does_support_bootstrapping())
+        self.assertTrue(SQVersion.from_str("11.1.1").does_support_bootstrapping())
+        self.assertTrue(SQVersion.from_str("11").does_support_bootstrapping())
+
+        self.assertFalse(SQVersion.from_str("9.9.9").does_support_bootstrapping())
+        self.assertFalse(SQVersion.from_str("9.9.9a").does_support_bootstrapping())
+        self.assertFalse(SQVersion.from_str("9.9").does_support_bootstrapping())
+        self.assertFalse(SQVersion.from_str("9").does_support_bootstrapping())
+        self.assertFalse(SQVersion.from_str("10.5").does_support_bootstrapping())
+        self.assertFalse(SQVersion.from_str("10").does_support_bootstrapping())
+
+        self.assertFalse(SQVersion.from_str("a").does_support_bootstrapping())
+        self.assertFalse(SQVersion.from_str("1.a").does_support_bootstrapping())
+
+    def test_str(self):
+        self.assertEqual(str(SQVersion.from_str("10.6")), "10.6")
+        self.assertEqual(str(SQVersion.from_str("9.9.9aa")), "9.9.9aa")
