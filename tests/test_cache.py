b090ea389b069e9fde0595ddd8da60d3258f6834
Sebastian Zumbrunn <sebastian.zumbrunn@sonarsource.com>
2025-03-07 15:59:53 +0100

    add cache classes

---
+++ tests/test_cache.py
@@ -0,0 +1,55 @@
+#
+# Sonar Scanner Python
+# Copyright (C) 2011-2024 SonarSource SA.
+# mailto:info AT sonarsource DOT com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+#
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+import pathlib
+import unittest
+import pyfakefs.fake_filesystem_unittest as pyfakefs
+
+from pysonar_scanner.cache import Cache, CacheFile
+import pysonar_scanner.cache as cache
+
+
+class TestCacheFile(unittest.TestCase):
+    def test_is_valid(self):
+        self.assertFalse(CacheFile(pathlib.Path("/tmp/none-existing-file"), checksum="123").is_valid())
+
+
+class TestCache(pyfakefs.TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
+
+    def test_if_cache_folder_is_created(self):
+        cache_path = pathlib.Path("/folder1/folder2/")
+
+        cache = Cache.create_cache(cache_path)
+        self.assertTrue(self.fs.exists(cache_path))
+        self.assertTrue(cache.cache_folder, cache_path)
+
+    def test_cache_constructor(self):
+        with self.assertRaises(FileNotFoundError):
+            Cache(pathlib.Path("/folder1/folder2/"))
+
+    def test_get_file(self):
+        cache = Cache.create_cache(pathlib.Path("/folder1/folder2/"))
+        cache_file = cache.get_file("test", "123")
+        self.assertEqual(cache_file.filepath, pathlib.Path("/folder1/folder2/test"))
+        self.assertEqual(cache_file.checksum, "123")
+
+    def test_get_default(self):
+        self.assertEqual(cache.get_default().cache_folder, pathlib.Path.home() / ".sonar-scanner")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,9 +17,10 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
+from io import BytesIO
 import unittest
 
-from pysonar_scanner.utils import remove_trailing_slash
+from pysonar_scanner.utils import remove_trailing_slash, calculate_checksum
 
 
 class TestUtils(unittest.TestCase):
@@ -28,3 +29,14 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(remove_trailing_slash(" test/ "), "test")
         self.assertEqual(remove_trailing_slash(" test / "), "test")
         self.assertEqual(remove_trailing_slash("test"), "test")
+
+
+class TestCalculateChecksum(unittest.TestCase):
+    def test_calculate_checksum(self):
+        self.assertEqual(
+            calculate_checksum(BytesIO(b"test")), "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+        )
+        self.assertEqual(
+            calculate_checksum(BytesIO(b"test test")),
+            "03ffdf45276dd38ffac79b0e9c6c14d89d9113ad783d5922580f4c66a3305591",
+        )
