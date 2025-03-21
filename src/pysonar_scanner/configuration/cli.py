aa9176654bdfc17d3e64f8eed3369631a50f3a46
Thomas Serre <118730793+thomas-serre-sonarsource@users.noreply.github.com>
2025-03-21 11:11:57 +0100

    SCANPY-130 Accept options of the form -Dsonar.*** (#156)

--- src/pysonar_scanner/configuration/cli.py
+++ src/pysonar_scanner/configuration/cli.py
@@ -44,50 +44,66 @@ class CliConfigurationLoader:
             "-t",
             "--token",
             "--sonar-token",
+            "-Dsonar.token",
             type=str,
             help="Token used to authenticate against the SonarQube Server or SonarQube Cloud",
         )
         parser.add_argument(
             "--sonar-project-key",
+            "-Dsonar.projectKey",
             type=str,
             help="Key of the project that usually corresponds to the project name in SonarQube",
         )
 
         parser.add_argument(
-            "-v", "--verbose", "--sonar-verbose", action="store_true", default=None, help="Increase output verbosity"
+            "-v",
+            "--verbose",
+            "--sonar-verbose",
+            "-Dsonar.verbose",
+            action="store_true",
+            default=None,
+            help="Increase output verbosity",
         )
 
         parser.add_argument(
             "--sonar-host-url",
+            "-Dsonar.host.url",
             type=str,
             help="SonarQube Server base URL. For example, http://localhost:9000 for a local instance of SonarQube Server",
         )
         parser.add_argument(
             "--sonar-region",
+            "-Dsonar.region",
             type=str,
             choices=["us"],
             help="The region to contact, only for SonarQube Cloud",
         )
-        parser.add_argument("--sonar-user-home", type=str, help="Base sonar directory, ~/.sonar by default")
+        parser.add_argument(
+            "--sonar-user-home", "-Dsonar.userHome", type=str, help="Base sonar directory, ~/.sonar by default"
+        )
 
         parser.add_argument(
             "--sonar-scanner-cloud-url",
+            "-Dsonar.scanner.cloudUrl",
             type=str,
             help="SonarQube Cloud base URL, https://sonarcloud.io for example",
         )
         parser.add_argument(
             "--sonar-scanner-api-url",
+            "-Dsonar.scanner.apiUrl",
             type=str,
             help="Base URL for all REST-compliant API calls, https://api.sonarcloud.io for example",
         )
         parser.add_argument(
             "--sonar-scanner-os",
+            "-Dsonar.scanner.os",
             type=str,
             choices=["windows", "linux", "macos", "alpine"],
             help="OS running the scanner",
         )
         parser.add_argument(
             "--sonar-scanner-arch",
+            "-Dsonar.scanner.arch",
             type=str,
             choices=["x64", "aarch64"],
             help="Architecture on which the scanner will be running",
@@ -95,65 +111,92 @@ class CliConfigurationLoader:
 
         parser.add_argument(
             "--skip-jre-provisioning",
+            "-Dsonar.scanner.skipJreProvisioning",
             action="store_true",
             default=None,
             help="If provided, the provisioning of the JRE will be skipped",
         )
         parser.add_argument(
-            "--sonar-scanner-java-exe-path", type=str, help="If defined, the scanner engine will be run with this JRE"
+            "--sonar-scanner-java-exe-path",
+            "-Dsonar.scanner.javaExePath",
+            type=str,
+            help="If defined, the scanner engine will be run with this JRE",
         )
         parser.add_argument(
-            "--sonar-scanner-java-opts", type=str, help="Arguments provided to the JVM when running the scanner"
+            "--sonar-scanner-java-opts",
+            "-Dsonar.scanner.javaOpts",
+            type=str,
+            help="Arguments provided to the JVM when running the scanner",
         )
 
         parser.add_argument(
             "--sonar-scanner-internal-dump-to-file",
+            "-Dsonar.scanner.internal.dumpToFile",
             type=str,
             help="Filename where the input to the scanner engine will be dumped. Useful for debugging",
         )
         parser.add_argument(
             "--sonar-scanner-internal-sq-version",
+            "-Dsonar.scanner.internal.sqVersion",
             type=str,
             help="Emulate the result of the call to get SQ server version.  Useful for debugging with --sonar-scanner-internal-dump-to-file",
         )
 
         parser.add_argument(
             "--sonar-scanner-connect-timeout",
+            "-Dsonar.scanner.connectTimeout",
             type=int,
             help="Time period to establish connections with the server (in seconds)",
         )
         parser.add_argument(
             "--sonar-scanner-socket-timeout",
+            "-Dsonar.scanner.socketTimeout",
             type=int,
             help="Maximum time of inactivity between two data packets when exchanging data with the server (in seconds)",
         )
         parser.add_argument(
             "--sonar-scanner-response-timeout",
+            "-Dsonar.scanner.responseTimeout",
             type=int,
             help="Time period required to process an HTTP call: from sending a request to receiving a response (in seconds)",
         )
 
         parser.add_argument(
             "--sonar-scanner-truststore-path",
+            "-Dsonar.scanner.truststorePath",
             type=str,
             help="Path to the keystore containing trusted server certificates, used by the Scanner in addition to OS and the built-in certificates",
         )
-        parser.add_argument("--sonar-scanner-truststore-password", type=str, help="Password to access the truststore")
+        parser.add_argument(
+            "--sonar-scanner-truststore-password",
+            "-Dsonar.scanner.truststorePassword",
+            type=str,
+            help="Password to access the truststore",
+        )
 
         parser.add_argument(
             "--sonar-scanner-keystore-path",
+            "-Dsonar.scanner.keystorePath",
             type=str,
             help="Path to the keystore containing the client certificates used by the scanner. By default, <sonar.userHome>/ssl/keystore.p12",
         )
-        parser.add_argument("--sonar-scanner-keystore-password", type=str, help="Password to access the keystore")
+        parser.add_argument(
+            "--sonar-scanner-keystore-password",
+            "-Dsonar.scanner.keystorePassword",
+            type=str,
+            help="Password to access the keystore",
+        )
 
-        parser.add_argument("--sonar-scanner-proxy-host", type=str, help="Proxy host")
-        parser.add_argument("--sonar-scanner-proxy-port", type=int, help="Proxy port")
-        parser.add_argument("--sonar-scanner-proxy-user", type=str, help="Proxy user")
-        parser.add_argument("--sonar-scanner-proxy-password", type=str, help="Proxy password")
+        parser.add_argument("--sonar-scanner-proxy-host", "-Dsonar.scanner.proxyHost", type=str, help="Proxy host")
+        parser.add_argument("--sonar-scanner-proxy-port", "-Dsonar.scanner.proxyPort", type=int, help="Proxy port")
+        parser.add_argument("--sonar-scanner-proxy-user", "-Dsonar.scanner.proxyUser", type=str, help="Proxy user")
+        parser.add_argument(
+            "--sonar-scanner-proxy-password", "-Dsonar.scanner.proxyPassword", type=str, help="Proxy password"
+        )
 
         parser.add_argument(
             "--sonar-project-base-dir",
+            "-Dsonar.projectBaseDir",
             type=str,
             help="Directory containing the project to be analyzed. Default is the current directory",
         )
